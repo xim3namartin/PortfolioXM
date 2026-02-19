f37c30859a3adb1d0c40aa80008f11e21a1ef19c
xim3namartin <ximenamartin33@gmail.com>
2026-02-18 21:32:14 -0300

    modified projects

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,22 +1,34 @@
 export const projects = [
   {
-    title: "E-commerce Full Stack App",
+    title: "Full Stack App",
     problem:
-      "Users needed a simple platform to browse and purchase products online.",
+      "A platform for movie reviews that allows users to easily browse.",
     solution:
-      "Developed a full stack application with authentication and payment integration.",
-    stack: ["React", "Node.js", "MongoDB"],
-    github: "https://github.com/yourrepo",
-    demo: "https://yourdemo.com"
+      "Developed a full stack application with authentication, responsive UI and a clean, modular structure.",
+    stack: ["JavaScript", "Node.js", "MySQL", "Express"],
+    github: "https://github.com/xim3namartin/Movie_reviews",
+    demo: "https://movie-reviews-gonn.onrender.com"
   },
   {
-    title: "Sales Analytics Dashboard",
+    title: "Front End Project",
     problem:
-      "Business stakeholders needed better visualization of sales KPIs.",
+      "Users need a useful and navigable platform to purchase products online.",
     solution:
-      "Created an interactive dashboard with data filtering and real-time insights.",
-    stack: ["React", "Python", "PostgreSQL"],
-    github: "https://github.com/yourrepo",
-    demo: "https://yourdemo.com"
+      "Created a modern app using React with reusable components, state management, and basic navigation.",
+    stack: ["React", "Vite", "Auth", "CRUD"],
+    github: "https://github.com/xim3namartin/Proyecto-React-25235-XM",
+    demo: "https://proyecto-react-25235-xm.vercel.app/"
+  },
+
+    {
+    title: "UX/UI Design mobile app",
+    problem:
+      "Need to obtain beauty products on a single platform.",
+    solution:
+      "Design an app for beauty products so that users get a clear, intuitive and visually appealing digital experience following modern UX/UI principles.",
+    stack: ["UX/UI", "Figma", "prototyping"],
+    github: "https://github.com/xim3namartin/ux-ui-final-proyect",
+    demo: "https://www.figma.com/design/6HrBn8u2zlcDkMDyustc3p/Proyecto-final?node-id=371-7&p=f",
   }
+
 ];
